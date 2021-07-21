be882344bcf07c878b604af7414caf080e9ab29c
JesNatTer <82204857+JesNatTer@users.noreply.github.com>
2021-07-21 12:10:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 from flask import Flask
 import requests
 
-
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def get_chuck_norris_jokes():
-
     api_url = "https://api.chucknorris.io/jokes/random"
     response = requests.get(api_url).json()
+    return '<pre style="word-wrap: break-word; white-space: pre-wrap;">{}</pre>'.format(response)
 
-    return "<strong>Random joke from chuck norris: </strong>" + response['value']
+
+@app.route('/categories', methods=['GET'])
+def get_categories():
+    cat = "https://api.chucknorris.io/jokes/categories"
+    response = requests.get(cat).json()
+    return '<pre style="word-wrap: break-word; white-space: pre-wrap;">{}</pre>'.format(response)
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
